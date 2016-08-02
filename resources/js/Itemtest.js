9e220601db03fe77f525ddf5b48a680d6fbb543e
hemantha.kumar <palamakulahemanth@gmail.com>
2016-08-02 21:27:35 +0530

    Test changes

--- resources/js/Itemtest.js
+++ resources/js/Itemtest.js
@@ -1,3 +1,4 @@
+var showAlert;
 function fnSaveUserAnswer(questionid, selectedoption)
 {
 	$.ajax({
@@ -10,6 +11,17 @@ function fnSaveUserAnswer(questionid, selectedoption)
 	});
 }
 
+function fnShowAlert()
+{
+	clearInterval(showAlert);
+
+	$('.alert-danger').fadeIn().delay(4000).fadeOut(400);
+			
+	setTimeout(function(){
+		$('.alert-warning').fadeIn().delay(4000).fadeOut(400);
+	}, 5000);
+}
+
 $('document').ready(function(){
    // More Info Audio
    setTimeout(function(){
@@ -19,70 +31,71 @@ $('document').ready(function(){
 		  // Attaches an event ended and it gets fired when current playing song get ended
 		   audioPlay.addEventListener('ended', function() {
 				$('.tonal-test-wrapper .tonal-test-view .option-view label').css('pointer-events','inherit');
-				
-				setTimeout(function(){
-					if(!$("input.custom-radio-button:checked").val()) {
-						
-						$('.alert-danger').fadeIn().delay(4000).fadeOut(400);
-						
-						setTimeout(function(){
-							$('.alert-warning').fadeIn().delay(4000).fadeOut(400);
-					
-						}, 5000);										
-					}else
-					{
-						fnSaveUserAnswer($("input.custom-radio-button:checked").attr("data-role-id"), $("input.custom-radio-button:checked").attr("data-role-option"));
-						
-						setTimeout(function(){
-							//$('.alert-success').fadeIn().delay(5000).fadeOut(400);
 
-							if((parseInt($("#hdnQuestionNo").val())+1) == arrQuestions.length)
-							{
-								$('.NextButtonWrapper').show();
-							}
-
-							var intNextQuestion = parseInt($("#hdnQuestionNo").val())+1;
-
-							if(arrQuestions.length > intNextQuestion)
-							{
-								$("#hdnQuestionNo").val(intNextQuestion);
-
-								$("#TestAudioData").attr('src', strBaseURL+arrQuestions[intNextQuestion].audiopath);
-
-								$("input.custom-radio-button").attr("checked",false);
-
-								/*for(var intCtr = 1; intCtr<= arrQuestions[intNextQuestion].optionscount; intCtr++)
-								{
-									$("#id_"+intCtr).attr('data-role-id', arrQuestions[intNextQuestion].id);
-
-									$strOldClass = $("#id_"+intCtr).attr('class');
-
-									$("#id_"+intCtr).removeClass($strOldClass);
-
-									$("#id_"+intCtr).addClass('radiobtn-custom-'+arrQuestions[intNextQuestion].optioncolor);
-
-									$strLblClass = $("#lbl_"+intCtr).attr("class");
-
-									$("#lbl_"+intCtr).removeClass($strLblClass);
-
-									$("#lbl_"+intCtr).addClass('btn-'+arrQuestions[intNextQuestion].optioncolor);
-
-								}*/
-								
-								$('.tonal-test-wrapper .tonal-test-view .option-view label').css('pointer-events','none');
-
-								audioPlay1 = document.getElementById('TestAudioData');
-
-								audioPlay1.play();
-
-								$("#h1QuestionCode").html(arrQuestions[intNextQuestion].questioncode);
-							}
-					
-						}, 5000);
-					}
-				}, 3000);
-				
+				showAlert = setInterval(function(){
+					fnShowAlert();
+				},3000);
 		   });
+
 	   });
    },6000);
+
+   $("input.custom-radio-button").bind('click', function()
+	{
+		if(!$("input.custom-radio-button:checked").val()) {
+			fnShowAlert();			
+		}else
+		{
+			clearInterval(showAlert);
+
+			fnSaveUserAnswer($("input.custom-radio-button:checked").attr("data-role-id"), $("input.custom-radio-button:checked").attr("data-role-option"));
+			
+			setTimeout(function(){
+
+				if((parseInt($("#hdnQuestionNo").val())+1) == arrQuestions.length)
+				{
+					$('.NextButtonWrapper').show();
+				}
+
+				var intNextQuestion = parseInt($("#hdnQuestionNo").val())+1;
+
+				if(arrQuestions.length > intNextQuestion)
+				{
+					$("#hdnQuestionNo").val(intNextQuestion);
+
+					$("#TestAudioData").attr('src', strBaseURL+arrQuestions[intNextQuestion].audiopath);
+
+					$("input.custom-radio-button").attr("checked",false);
+
+					for(var intCtr = 1; intCtr<= arrQuestions[intNextQuestion].optionscount; intCtr++)
+					{
+						$("#id_"+intCtr).attr('data-role-id', arrQuestions[intNextQuestion].id);
+
+						$strOldClass = $("#id_"+intCtr).attr('class');
+
+						$("#id_"+intCtr).removeClass($strOldClass);
+
+						$("#id_"+intCtr).addClass('radiobtn-custom-'+arrQuestions[intNextQuestion].optioncolor);
+
+						$strLblClass = $("#lbl_"+intCtr).attr("class");
+
+						$("#lbl_"+intCtr).removeClass($strLblClass);
+
+						$("#lbl_"+intCtr).addClass('btn-'+arrQuestions[intNextQuestion].optioncolor);
+
+					}
+					
+					$('.tonal-test-wrapper .tonal-test-view .option-view label').css('pointer-events','none');
+
+					audioPlay1 = document.getElementById('TestAudioData');
+
+					audioPlay1.play();
+
+					$("#h1QuestionCode").html(arrQuestions[intNextQuestion].questioncode);
+				}
+		
+			}, 5000);
+		}
+	});
+
 });
