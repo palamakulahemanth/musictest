43946b7f5d664212d46a9350ac43858910cbe8f7
hemantha.kumar <palamakulahemanth@gmail.com>
2016-08-06 21:01:52 +0530

    Thank you view

---
+++ application/views/thankyou.php
@@ -0,0 +1,20 @@
+<?=$Header;?>
+<!-- Body content goes here -->
+<section class="intro-wrapper nextbranch-view">
+		<div class="container">
+			<div class="row">
+				<div class="thankyou-view">
+					<h1>
+						You have completed the AIMS Tonal Memory exercise. You correctly answered <span><?=$CorrectAns;?></span> of the <span><?=$NoOfQtsAttempted;?></span> items.
+					</h1>
+					<h2>
+						This indicates that you have <span>average</span> Tonal Memory ability. 
+					</h2>
+				</div>
+            </div>
+		</div>
+</section>
+<!-- Body content ends here -->
+<!-- JS files will load here -->
+
+<?=$Footer;?>
